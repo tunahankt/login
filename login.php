1393bcdad0f98b7813b3877cb03da08bf48498a7
Tunahan <tunahan.kt74@gmail.com>
2023-09-04 09:41:51 +0300

    login

--- login.php
+++ login.php
@@ -2,87 +2,90 @@
 <html lang="tr">
 <head>
     <meta charset="UTF-8">
-    <title>GÜVENLİK GÖREVLİSİ KAT ZİYARET TAKİBİ</title>
+    <title>AKTIP</title>
     <style>
-        * {
-            box-sizing: border-box;
-        }
+    * {
+      box-sizing: border-box;
+    }
 
-        body {
-            font-family: sans-serif;
-            background-color: #F6F7FA;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-        }
+    body {
+      font-family: sans-serif;
+      background-color: #F6F7FA;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+    }
 
-        .form-container {
-            width: 600px; 
-            padding: 30px;
-            background-color: white;
-            border: 1px solid rgb(235, 235, 235);
-            border-radius: 15px;
-            box-shadow: 0 3px 5px 0 rgba(240, 240, 240, 0.3);
-            animation: transitionIn-Y-over 0.5s;
-        }
+    .form-container {
+      width: 900px;
+      padding: 30px;
+      background-color: white;
+      border: 1px solid rgb(235, 235, 235);
+      border-radius: 15px;
+      box-shadow: 0 3px 5px 0 rgba(240, 240, 240, 0.3);
+      animation: transitionIn-Y-over 0.5s;
+    }
 
-        h1 {
-            text-align: center;
-            font-size: 40px;
-            margin-bottom: 50px;
-        }
+    h1 {
+      text-align: center;
+      font-size: 30px;
+      margin-bottom: 50px;
+    }
 
-        .form-group {
-            margin-bottom: 20px;
-            text-align: center;
-        }
+    .form-group {
+      margin-bottom: 20px;
+      margin-left: 5px;
+      text-align: left;
+    }
 
-        .form-label {
-            color: rgb(44, 44, 44);
-            text-align: center;
-            font-size: 15px;
-            margin-bottom: 10px;
-            font-weight: bold;
-        }
+    .form-label {
+      color: rgb(44, 44, 44);
+      text-align: left;
+      font-size: 18px;
+      margin-bottom: 20px; 
+      font-weight: bold;
+    }
 
-        .form-input,
-        .form-textarea {
-            width: 100%;
-            padding: 10px;
-            border-radius: 5px;
-            border: 1px solid #ccc;
-        }
+    .form-input,
+    .form-textarea {
+      width: 95%;
+      padding: 12px;
+      border-radius: 5px;
+      border: 2px solid #ccc;
+    }
 
-        .form-submit {
-            background-color: #6d8493;
-            color: #fff;
-            padding: 10px 20px;
-            font-weight: bold;
-            border: none;
-            border-radius: 3px;
-            cursor: pointer;
-            display: block;
-            margin: 0 auto;
-        }
+    .form-submit {
+      background-color: #6d8493;
+      color: #fff;
+      padding: 10px 20px;
+      font-weight: bold;
+      border: none;
+      border-radius: 3px;
+      cursor: pointer;
+      display: block;
+      margin: 0 auto;
+    }
 
-        .form-submit:hover {
-            background-color: #1874c2;
-        }
+    .form-submit:hover {
+      background-color: #1874c2;
+    }
 
-        #gorus {
-            max-height: 300px;
-            resize: none ;
-            width: 100%;
-            height: 110px;
-        }
-    </style>
+    .form-logo {
+      width: 30%;
+      margin: 0 auto;
+    }
+
+    .form-logo img {
+      width: 30%;
+    }
+  </style>
 </head>
 <body>
 
-
-        <div class="form-container">
+<form action="qr.php" method="post">
+    <div class="form-container">
         <img src="logo.png" alt="Logo" class="form-logo" style="display: block; margin: 0 auto;">
         <br style="margin-top: 20px;">  
 
@@ -99,6 +102,7 @@
         <input type="submit" value="Giriş Yap" class="form-submit">
 
     </div>
+</form>
 
 </body>
 <?php
